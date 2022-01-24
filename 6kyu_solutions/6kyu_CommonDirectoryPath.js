7f90caeacb561523b657da8d9afdef6a7a348ca2
Carlo <carlonyc001@gmail.com>
2022-01-23 20:13:17 -0500

    common directory

---
+++ 6kyu_solutions/6kyu_CommonDirectoryPath.js
@@ -0,0 +1,29 @@
+// Returns the commom directory path for specified array of full filenames.
+
+//  @param {array} pathes
+//  @return {string}
+// Examples:
+
+//   ['/web/images/image1.png', '/web/images/image2.png']  => '/web/images/'
+//   ['/web/assets/style.css', '/web/scripts/app.js',  'home/setting.conf'] => ''
+//   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
+//   ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
+
+function getCommonDirectoryPath(pathes) {
+    let output = pathes[0];
+  
+    for(let i=1; i<pathes.length; i++){
+      for(let j=0; j<pathes[i].length; j++){
+        if(output[j] != pathes[i][j]){
+          output = output.substring(0,j);
+        }
+      }
+    }
+    
+    let indexer = output.lastIndexOf("/");
+    if(indexer != -1){
+      output = output.substring(0,indexer+1);
+    }
+  
+    return output;
+  }
