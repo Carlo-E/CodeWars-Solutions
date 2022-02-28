4ddf2c6df16e6190861f44afea2ac6b484504cfa
Carlo <carlonyc001@gmail.com>
2022-02-28 18:39:56 -0500

    closer number II

--- Hackerrank/strongPassword.js
+++ Hackerrank/strongPassword.js
@@ -48,4 +48,47 @@ function minimumNumber(n, password) {
 
   return Math.max(count, 6 - password.length)
 }
-console.log(minimumNumber(3, "Ab1"));
+// console.log(minimumNumber(3, "Ab1"));
+
+let films = [
+    {
+      id: 70111470,
+      title: "Die Hard",
+      boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+      uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+      rating: 4.0,
+    },
+    {
+      id: 654356453,
+      title: "Bad Boys",
+      boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+      uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+      rating: 5.0,
+    },
+    {
+      id: 65432445,
+      title: "The Chamber",
+      boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+      uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+      rating: 4.0,
+      bookmark: [],
+    },
+    {
+      id: 675465,
+      title: "Fracture",
+      boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+      uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+      rating: 5.0,
+      bookmark: [{ id: 432534, time: 65876586 }],
+    },
+  ],
+  idAndTitlePairs = [];
+
+films.forEach((film) => {
+  idAndTitlePairs.push({ id: film.id, title: film.title });
+});
+
+console.log(idAndTitlePairs)
+  
+
+
