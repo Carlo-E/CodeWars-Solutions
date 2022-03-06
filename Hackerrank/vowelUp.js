33b8d02e307ac9108d5171ee89a8a40b959781d0
Carlo <carlonyc001@gmail.com>
2022-03-05 20:44:28 -0500

    vowel up

--- Hackerrank/vowelUp.js
+++ Hackerrank/vowelUp.js
@@ -1,9 +1,11 @@
 function vowelUp(str){
     let vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+    console.log(vowels)
 
-    if(vowels.has(str[0].toLowerCaser())){
+    if(vowels.has(str[0].toLowerCase())){
         return str.toUpperCase()
     } else {
         return str
     }
 }
+console.log(vowelUp('ABacus'))
