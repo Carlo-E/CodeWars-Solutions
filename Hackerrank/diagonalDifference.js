e057703ab415a619a441be7e087015939867cd0d
Carlo <carlonyc001@gmail.com>
2022-03-10 21:48:48 -0500

    diagonal v2

--- Hackerrank/diagonalDifference.js
+++ Hackerrank/diagonalDifference.js
@@ -1,29 +1,39 @@
 // https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
 
-function diagonalDifference(arr) {
-  let sumRight = 0;
-  let sumLeft = 0;
+// function diagonalDifference(arr) {
+//   let sumRight = 0;
+//   let sumLeft = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for(let j = i; j < arr.length; j++){
+//         sumRight+= arr[i][j]
+//         break;
+//     }
+//   }
+//   let reverse = [];
+//   arr.map(el => reverse.push(el.reverse()));
+//   console.log(reverse)
+//   for(let i = 0; i < reverse.length; i++){
+//     for(let j = i; j < reverse.length; j++){
+//         console.log(reverse[i][j])
+//         sumLeft+= reverse[i][j]
+//         break;
+//     }
+//   }
+//   return Math.abs(sumRight - sumLeft);
+// }
+
+const diagonalDifference = (arr) => {
+  let left = 0;
+  let right = 0;
   for (let i = 0; i < arr.length; i++) {
-    for(let j = i; j < arr.length; j++){
-        sumRight+= arr[i][j]
-        break;
-    }
+    left += arr[i][i];
+    right += arr[i][arr.length - 1 - i];
   }
-  let reverse = [];
-  arr.map(el => reverse.push(el.reverse()));
-  console.log(reverse)
-  for(let i = 0; i < reverse.length; i++){
-    for(let j = i; j < reverse.length; j++){
-        console.log(reverse[i][j])
-        sumLeft+= reverse[i][j]
-        break;
-    }
-  }
-  return Math.abs(sumRight - sumLeft);
-}
+  return Math.abs(left - right);
+};
 console.log(
   diagonalDifference([
-    [1, 2, 3,],
+    [1, 2, 3],
     [4, 5, 6],
     [9, 8, 9],
   ])
