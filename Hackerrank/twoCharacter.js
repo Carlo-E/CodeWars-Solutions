236965ad99c7f86a9c641ccd41595b26f61f3c37
Carlo <carlonyc001@gmail.com>
2022-03-14 23:08:05 -0400

    two character first passing first test

--- Hackerrank/twoCharacter.js
+++ Hackerrank/twoCharacter.js
@@ -1,24 +1,30 @@
 // https://www.hackerrank.com/challenges/three-month-preparation-kit-two-characters/problem?h_r=internal-search
 
-function alternate(s) {
-    // Write your code here
-    s = s.split("")
-    // console.log(s)
-    let arr = []
-    for(let i = 0; i < s.length; i++){
-        if(s[i] == s[i + 1]){
-            arr.push(s[i])
-        }
-
+const removeDuplicates = (s) => {
+  // Write your code here
+  s = s.split("");
+  // console.log(s)
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == s[i + 1]) {
+      arr.push(s[i]);
     }
-    
-    let arr2 = s.filter(el => el != arr[0])
+  }
 
-    console.log(arr2)
-    
-    
-
-
-
+  return s.filter((el) => el != arr[0]);
+};
+function alternate(s) {
+  let arr = removeDuplicates(s);
+  let arr2 = [];
+  // bcfbcb
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != arr[i + 2]) {
+      arr2.push(arr[i + 2]);
+      break;
+    }
+  }
+  return arr.filter((el) => el != arr2[0]).join("");
 }
-console.log(alternate('beabeefeab'))
+console.log(alternate("asdcbsdcagfsdbgdfanfghbsfdab"));
+
+// console.log(removeDuplicates("aabcabc"));
