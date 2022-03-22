3faa963363d68ad011f873ae324fe3accabd7178
Carlo <carlonyc001@gmail.com>
2022-03-21 22:58:57 -0400

    max stack

--- Hackerrank/sumConsecutive.js
+++ Hackerrank/sumConsecutive.js
@@ -11,6 +11,12 @@ let matrix2 = [
   [3, 7, 23],
 ];
 
+let matrix3 = [
+  [3, 6, 8],
+  [2, 0, 9],
+  [3, 6, 1],
+];
+
 const maxInArray = (arr, num = 1) => {
   let maxSum = -Infinity;
   for (let i = 0; i < arr.length - num + 1; i++) {
@@ -45,4 +51,24 @@ function maxNConsecutiveElementsInMatrix(matrix, num) {
   }
   return maxElement;
 }
-console.log(maxNConsecutiveElementsInMatrix(matrix2, 2));
+// console.log(maxNConsecutiveElementsInMatrix(matrix2, 2));
+
+function maxStacked(matrix, stackSize = 2) {
+  let maxStack = -Infinity;
+
+  for (let i = 0; i <= matrix.length - stackSize; i++) {
+    //  console.log(matrix.length - stackSize)
+    for (let j = 0; j < matrix[i].length; j++) {
+        // console.log(matrix[i].length)
+      let columnArr = [];
+      for (let m = 0; m < stackSize; m++) {
+        columnArr.push(matrix[i + m][j]);
+        // 
+      }
+      let currMax = columnArr.reduce((a, b) => a + b);
+      if (currMax > maxStack) maxStack = currMax;
+    }
+  }
+  return maxStack;
+}
+console.log(maxStacked(matrix3))
