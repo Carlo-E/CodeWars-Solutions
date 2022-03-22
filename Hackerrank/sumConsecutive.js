89d7f716434d8f3024df2100c9840b3c131d6212
Carlo <carlonyc001@gmail.com>
2022-03-21 22:01:47 -0400

    max cons

--- Hackerrank/sumConsecutive.js
+++ Hackerrank/sumConsecutive.js
@@ -1,22 +1,24 @@
 // what is the max sum from the consecutive number of elements
 
-const maxConSum = (arr, num) => {
-    let maxSum = -Infinity
-    for(let i = 0; i < arr.length - num + 1; i++){
-        // 6 - 3 + 1 = 4
-        let currSum = arr.slice(i, num + i).reduce((a,b) => a + b);
-        console.log(currSum)
-        if(currSum > maxSum) {
-            maxSum = currSum
-        }
+const maxInArray = (arr, num = 1) => {
+  let maxSum = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    let currSum = arr.slice(i, num + i).reduce((a, b) => a + b);
+    if (currSum > maxSum) {
+      maxSum = currSum;
     }
-    // return maxSum
-    // 0, 3 => 4, 5, 1 = 10
-    // 1, 4 => 5, 1, 3 = 9
-    // 2, 5 => 1, 3, 2 = 6
-    // 3, 6 => 3, 2, 5 = 10
-    // 4, 
+  }
+  return maxSum;
+};
+// console.log(maxConSum([4, 5, 1, 3, 2, 5], 1))
 
+
+
+function maxNConsecutiveElementsInMatrix(matrix, num) {
+    // max el in the matrix
+  let maxElement = -Infinity;
+  for (let i = 0; i < matrix.length; i++) {
+    let currSum = maxInArray(matr);
+  }
 }
-console.log(maxConSum([4, 5, 1, 3, 2, 5], 3))
-
+console.log(maxNConsecutiveElementsInMatrix());
