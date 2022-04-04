e3f8a5eb163a1e3527e9a67451985bcf264cf44c
Carlo <carlonyc001@gmail.com>
2022-04-04 17:42:07 -0400

    grade book

---
+++ 8kyu_solutions/8kyu_GradeBook.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/grasshopper-grade-book
+
+function getGrade (s1, s2, s3) {
+    var avg = (s1 + s2 + s3)/3;
+    switch(true) {
+      case (avg >= 90):
+        return 'A';
+      case ( avg >= 80):
+        return 'B';
+      case ( avg >= 70):
+        return 'C';
+      case ( avg >= 60):
+        return 'D'; 
+      default:
+        return 'F';   
+    }
+   }
+   
