234ad3cf6fe8d1e13eb8463f9b647ae8fc8c15f9
Carlo <carlonyc001@gmail.com>
2022-04-04 18:01:45 -0400

    messi goals

---
+++ 8kyu_solutions/8kyu_MessiGoals.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/grasshopper-messi-goals-function
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
