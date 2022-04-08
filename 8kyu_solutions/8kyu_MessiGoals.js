390e88c006a8126021a4bd3d6591540481b375cb
Carlo <carlonyc001@gmail.com>
2022-04-08 18:36:30 -0400

    print stars

--- 8kyu_solutions/8kyu_MessiGoals.js
+++ 8kyu_solutions/8kyu_MessiGoals.js
@@ -4,3 +4,4 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // code goes here
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
---
+++ Hackerrank/printStars.js
@@ -0,0 +1,9 @@
+function printStars(N) {
+  // let str = []
+  //   for(let i = 0; i < N; i++){
+  //      str.push('*')
+  //   }
+  // return str.join("")
+  return "*".repeat(N);
+}
+console.log(printStars(5));
